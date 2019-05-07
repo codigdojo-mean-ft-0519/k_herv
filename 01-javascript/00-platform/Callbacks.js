e836026d38fc7922c0faa59909f141a5c16d2137
Kent Hervey <kent.hervey@gmail.com>
2019-05-07 09:19:30 -0500

    hunting-github in-progress

---
+++ 01-javascript/00-platform/Callbacks.js
@@ -0,0 +1,97 @@
+
+
+// var ninja = 'Libby';
+// setTimeout( function (){ console.log(ninja); }, 2000 ); // run the function defined after 2000 milliseconds
+// console.log(ninja);
+
+// console.log("NOW: ");
+// console.log("Declaring and assigning variable 'ninja'.");
+// var ninja = 'Libby';
+// setTimeout( function myCallbackFunction(){
+//   console.log("LATER: ")
+//   console.log(ninja, "LATER"); }, 4000
+// );
+// console.log("Printing ninja value.");
+// console.log(ninja, "NOW");
+
+
+var a = 2;
+var b = function() { console.log("something"); };
+
+
+// function doSomething(x) {
+//   console.log(typeof(x));
+// }
+
+
+// doSomething(a);
+// doSomething(b);
+
+function doSomething(possibleCallback) {
+    if (typeof(possibleCallback) === 'function'){
+      console.log('possibleCallback is a callback!');
+      possibleCallback(); //we can invoke the callback!
+    }
+    else {
+      console.log('possibleCallback: ', possibleCallback, ' is not a callback function.');
+    }
+  }
+  doSomething(function myCallback(){ console.log('yes, I am a callback!') });
+  doSomething('string');
+
+//it took some staring, but I figured out the above
+
+console.log("\n\n");
+
+function makePasta(pasta, makeSauce) {
+    console.log("Boiling water");
+    console.log("Putting " + pasta + " pasta in the water");
+    // create a variable for sauce!
+    var sauce = makeSauce();          // invoke makeSauce, our callback
+    console.log("Mixing sauce");
+    console.log("Pasta is done!");
+    return pasta + " Pasta with " + sauce + " sauce! Voila!";
+  }
+  function makePesto() {
+    console.log("Making Pesto");
+    return "pesto";
+  }
+  function makeAlfredo() {
+    console.log("Making Alfredo");
+    return "alfredo";
+  }
+  // we pass the whole makePesto recipe to makePasta!
+  console.log(makePasta("Penne", makePesto));
+  // notice lack of parentheses after makePesto.
+  // Remember: we want to pass the function, not execute it and pass a return value.
+  console.log(makePasta("Farfalle", makeAlfredo));
+
+
+
+// assuming you have jQuery
+$(document).ready(function(){
+    var favoritePokemon;
+    // the given link is for Bulbasaur's information
+    $.get("https://pokeapi.co/api/v2/pokemon/1/", function(data){     
+        favoritePokemon = data.name;
+    });
+    console.log("Got my favorite Pokemon", favoritePokemon);      // Predict the output!
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
