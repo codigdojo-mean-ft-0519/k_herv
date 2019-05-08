b01f8655b58cdf1a4f99a9c2605bcd6ce50ef0e5
Kent Hervey <kent.hervey@gmail.com>
2019-05-08 09:49:23 -0500

    first commit nina-class-i

---
+++ 01-javascript/03-js-oop/01-ninja-class-i.js
@@ -0,0 +1,20 @@
+// Assignment: Ninja Class
+// Create a new object constructor called Ninja with the following attributes:
+
+// name
+// health
+// speed (private)
+// strength (private)
+// Speed and strength should be 3 by default. Health should be 100 by default.
+
+// Ninja should have the following methods:
+
+// sayName() - This should log that Ninja's name to the console.
+// showStats() - This should show the Ninja's name, strength, speed, and health.
+// drinkSake() - This should add +10 Health to the Ninja
+// Example Outputs
+// var ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// // -> "My ninja name is Hyabusa!"
+// ninja1.showStats();
+// // -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
