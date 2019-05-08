9d34581203bee63b69ff6d49c7e58021fbedd849
Kent Hervey <kent.hervey@gmail.com>
2019-05-08 15:49:49 -0500

    Platform Code Samples

--- 01-javascript/03-js-oop/codeFromPlatformSamples.js
+++ 01-javascript/03-js-oop/codeFromPlatformSamples.js
@@ -191,11 +191,250 @@ console.log(myCarInstance.odometer);
 // but we can read it with our getter function
 console.log(myCarInstance.readOdometer());
 
+//-----------
+//learning ES6 constructors
+console.log("\nstartES6\n")
+// class Dot {
+//   constructor(x, y) {
+//       this.x = x;
+//       this.y = y;
+//       console.log("You created a Dot!");
+//   }
+// }
+// const dot1 = new Dot(10, 10);
+
+
+// ES5 way
+// function Dot(x, y) {
+//   this.x = x;
+//   this.y = y;
+//   this.showLocation=function() {
+//       console.log("This Dot is at x " + this.x + " and y " + this.y);
+//     }
+
+    //var Person= function(name) {  ....versus....  function Person(name, age) 
+    // when creating a function that function is hoisted to the top of its scope. when assigning a function to a variable, the variable is hoisted, but the assignment is not. 
+    //const add = (num1, num2) => num1 + num2; an ES6 anonymous function...add is just the variable name
+    // function add(num1, num2) {
+    //   return num1 + num2;
+    // }
+    
+// }
+// Dot.prototype.showLocation = function() {
+//   console.log("This Dot is at x " + this.x + " and y " + this.y);
+// }
+// var dot1 = new Dot(55, 20);
+// console.log(dot1.showLocation);
+//ES6 way
+// class Dot {
+//   constructor(x, y) {
+//       this.xt = x;
+//       this.yt = y;
+//   }
+//   showLocation() {
+//       // ES6 String Interpolation! Note that the string is enclosed in backticks, not quotes.
+//       console.log(`This Dot is at x ${this.x} and y ${this.y}`);
+//   }
+// }
+// const dot2 = new Dot(5, 13);
+// console.log("dot2 has xt value of " + dot2.xt);
+
+// const dot3 = new Dot(99, 13);
+// console.log("dot3 has xt value of " + dot3.xt);
+
+// dot2.showLocation();
 
 
 
+// class Dot {
+//   constructor(x, y) {
+//       this.xt = x;
+//       this.yt = y;
+//   }
+//   // prototype method
+//   showLocation() {
+//       console.log(`This Dot is at x ${this.xt} and y ${this.yt}`);
+//       return 
+//   }
+//   // static method
+//   static getHelp() {
+//       console.log("This is a Dot class, for created Dots! A Dot takes x and y coordinates, type 'new Dot' to create one!");
+//   }
+// } 
+// const dot3 = new Dot(4, 2);
+// // we can see showLocation from our instance...
+// dot3.showLocation();
+
+// console.log(dot3.showLocation);
+// // but we can't see getHelp
+// console.log(dot3.getHelp);
+// // however we can call getHelp this way:
+// Dot.getHelp();
+
+// console.log(dot3.xt);
+
+//----------------------
+//Inheritance and Super
+
+// parent Dot class
+// class Dot {
+//   constructor(x, y) {
+//       this.xt = x;
+//       this.yt = y;
+//   }
+//   showLocation() {
+//       console.log(`This Dot is at x ${this.xt} and y ${this.yt}`);
+//   }
+// }
+// // child Circle class
+// class Circle extends Dot {
+//   constructor(xt, yt, radius) {
+//       super(xt, yt);
+//       this.radiust = radius;
+//   }
+// }
+// // we can now create Circles
+// const circle1 = new Circle(33, 33, 33);
+// // same attributes as a Dot, plus a radius
+// console.log(circle1);
+// // and Circles can access Dot methods
+// circle1.showLocation();
+
+//-------------------------------------------
+// parent Dot class
+// class Dot {
+//   constructor(x, y) {
+//       this.x = x;
+//       this.y = y;
+//   }
+//   showLocation() {
+//       console.log(`This ${ this.constructor.name } is at x ${this.x} and y ${this.y}`);
+//   }
+// }
+// child Circle class
+// class Circle extends Dot {
+//   constructor(x, y, radius) {
+//       super(x, y);
+//       this.radius = radius;
+//   }
+// }
 
 
+
+// const dot3 = new Dot(4, 2);
+
+// const roundthing = new Circle(4, 2,52);
+
+// console.log(roundthing);
+
+// roundthing.showLocation();
+
+// ///----------------------------
+
+// // parent Dot class
+// class Dot {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+//     showLocation() {
+//         console.log(`This ${ this.constructor.name } is at x ${ this.x } and y ${ this.y }`);
+//     }
+//     // simple method in the parent class
+//     parentFunction(){
+//         return "This is coming from the parent!";
+//     }
+// }
+// // child Circle class
+// class Circle extends Dot {
+//     constructor(x, y, radius) {
+//         super(x, y);
+//         this.radius = radius;
+//     }
+//     // simple function in the child class
+//     childFunction() {
+//         // by using super, we can call the parent method
+//         const message = super.parentFunction();
+//         console.log(message);
+//     }
+// }
+// const circle = new Circle(1, 2, 3);
+// circle.childFunction();
+
+
+//parent Dot class
+class Dot {
+  constructor(x, y) {
+      this.xt = x;
+      this.yt = y;
+  }
+  showLocation() {
+      console.log(`This ${ this.constructor.name } is at x ${ this.xt } and y ${ this.yt }`);
+  }
+  // simple method in the parent class
+  parentFunction(){
+      return "This is coming from the parent!";
+  }
+}
+// child Circle class
+class Circle extends Dot {
+  constructor(xt, yt, radius) {
+      super(xt, yt);
+      this.radius = radius;
+  }
+  // simple function in the child class
+  childFunction() {
+      // by using super, we can call the parent method
+      const message = super.parentFunction();
+      console.log(message);
+  }
+}
+const circle = new Circle(1, 2, 3);
+circle.childFunction();
+
+//Getters and Setters
+console.log("start getters and setters")
+
+// class Pizza {
+//   constructor(radius, slices) {
+//       this.radiust = radius;
+//       this._slicest = slices;
+//   }
+//   get slices () { 
+//       return this._slicest; 
+//   }
+//   set slices (slices) { 
+//       this._slicest = slices;
+//   }
+// };
+
+// const pie = new Pizza(12, 6);
+// console.log(pie.slices);     // we use the getter to determine how many slices we have
+// pie.slices = 12;             // the setter will change the number of slices
+// console.log(pie.slices);     // use the getter again to make sure our slices have doubled
+
+// console.log("here" + pie._slicest);
+
+// If we did not differentiate the attribute name from the getter and setter names, the above code would have put us into an infinite loop, and we would have gotten a RangeError: Maximum call stack size exceeded. It does not matter how you differentiate the names of the attributes from the Getters and Setters, but preceding the attribute with an underscore is convention.
+
+// Custom Getters
+// Using these same patterns, we can create custom Getters. Consider the following snippet:
+
+// class Circle{
+//   constructor(x, y, radius) {
+//       this.xt = x;
+//       this.yt = y;
+//       this.radiust = radius;
+//   }
+//   get diameter() {
+//       return this.radiust * 2;
+//   }
+// }
+// const circle1 = new Circle(1, 2, 5);
+// console.log(circle1.diameter);
+
+
+// Now our Circles have a diameter attribute even though we didn't explicitly add it to the constructor.
 
 
 
