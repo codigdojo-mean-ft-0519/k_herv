0e387ae46eaa2f8dcf97782e5060c6ee946c366e
Kent Hervey <kent.hervey@gmail.com>
2019-05-09 09:43:28 -0500

    test.js first commit

---
+++ 02-node/00-platform/test.js
@@ -0,0 +1,5 @@
+//created this file per Node NPM tab instructions to run from nodemon on terminal
+
+
+console.log('I am running from node');
+console.log('platform said make a change...this is a change');
