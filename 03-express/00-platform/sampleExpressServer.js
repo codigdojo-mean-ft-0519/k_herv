260d9ba9275de995b8d73943882bfc18ca7826f3
Kent Hervey <kent.hervey@gmail.com>
2019-05-11 11:17:25 -0500

    added comments and app.listen

---
+++ 03-express/00-platform/sampleExpressServer.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+var session = require('express-session');
+
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (request, response) {
+  console.log('text here tests whether route was entered');
+  if (request.session.count==undefined){request.session.count = 0;} //may be needed if using session
+  response.render('filename', {key: value});  //only one object can be passed...this is optional...remove or convert {key: value}
+});
+app.post('/route', function (request, response) { 
+    //action from the form
+  response.redirect('/');
+});
+
+app.listen(port, () => console.log(`express server listening on port ${port}`));  //port defined above as port = process.env.PORT || 8000;
