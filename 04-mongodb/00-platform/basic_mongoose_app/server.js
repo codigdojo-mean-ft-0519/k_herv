cc33070da43a7dd43771acd5856e6d08a46a40d4
Kent Hervey <kent.hervey@gmail.com>
2019-05-14 15:01:47 -0500

    delete record added

--- 04-mongodb/00-platform/basic_mongoose_app/server.js
+++ 04-mongodb/00-platform/basic_mongoose_app/server.js
@@ -46,9 +46,29 @@ mongoose.model('User', UserSchema); // We are setting this Schema in our Models 
 var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
 
 //Display the index
+
+   // User.remove({_id: 'insert record unique id here'}, function(err){
 app.get("/", function (request, response) {
-    User.find({})
-        .then(users => response.render('index', {users: users }))
+    User.find()
+        .then(function(users) { 
+            console.log("asd");
+            console.log(users);
+            
+            response.render('index', {users: users }) })
+
+
+        //.then(users => response.render('index', {users: users }))
+            //also works as:
+            //.then(function(users) { 
+             //   console.log("asd");
+             //   console.log(users);
+                
+            //    response.render('index', {users: users }) })
+        //response => response.json()
+        //is the same as function(response) { return response.json() }
+        //(users => response.render('index', {users: users }))
+        //is the same as 
+        //function(users) { response.render('index', {users: users }) }
       // This is the method that finds all of the users from the database
       // Notice how the first parameter is the options for what to find and the second is the
       //   callback function that has an error (if any) and all of the users
@@ -56,25 +76,19 @@ app.get("/", function (request, response) {
       //   happen inside of this callback for it to be synchronous 
       // Make sure you handle the case when there is an error, as well as the case when there is no error
       .catch(console.log());
-
     })
 
     //dummyArray=[{name: "bob, age: 45"}, {name:"fred", age: 33}];
     //response.render('index',{users:dummyArray});
+    //User.findOne({age:5})
+//from Jason lecture on this tutorial.....platform way would not be a catch or then....
+//User.findOne({}, function(err, user)
 
-
-
-
-
-
-// Use native promises (only necessary with mongoose versions <= 4)
-mongoose.Promise = global.Promise;
 
 app.post('/users', function(request, response) {
     console.log("next line is console log post requst.body")
     console.log("POST DATA", request.body);
     // This is where we would add the user from request.body to the database.
-
     var user = new User({name: request.body.name, age: request.body.age});
     // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
     user.save(function(err) {
@@ -88,6 +102,19 @@ app.post('/users', function(request, response) {
     })
 })
 
+
+//User.remove({_id: 'insert record unique id here'}, function(err){
+app.get("/delete", function (request, response) {
+    User.remove({name: "Clark"})
+        .then(function(users) { 
+            console.log("asd");
+            console.log(users);
+            
+            response.redirect('/' )} )
+
+
+      .catch(console.log())
+    })
 
 
 // listening for incoming connection on port
