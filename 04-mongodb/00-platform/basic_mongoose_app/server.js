941f02e951b34aee9677c051acf9134a7de63fa9
Kent Hervey <kent.hervey@gmail.com>
2019-05-14 10:00:52 -0500

    complete--updates and displays dbase

--- 04-mongodb/00-platform/basic_mongoose_app/server.js
+++ 04-mongodb/00-platform/basic_mongoose_app/server.js
@@ -1,14 +1,14 @@
 
 //basic_mongoose_app from platform
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 
 // const port = process.env.PORT || 8000;
 // const { PORT: port = 8000 } = process.env;
 const { env: { PORT: port = 8000 } } = process;
-//const { Schema } = mongoose;
+const { Schema } = mongoose;
 const app = express();
 
 var session = require('express-session');  //reference for session: 
@@ -33,16 +33,59 @@ app.set('views', path.join(__dirname, 'views'));
 //This allows us to pass text as URL encoded data and exposes the resulting object (containing the keys and values) on request.body.
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
+//   our db in mongodb -- this should match the name of the db you are going to use for your project.
+mongoose.connect('mongodb://localhost/basic_mongoose'); //basic_mongoose is the name of the database we will pick
+
+var UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+   })
+
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+
 //Display the index
 app.get("/", function (request, response) {
-    //Code goes here
-    response.render('index');
-});
+    User.find({})
+        .then(users => response.render('index', {users: users }))
+      // This is the method that finds all of the users from the database
+      // Notice how the first parameter is the options for what to find and the second is the
+      //   callback function that has an error (if any) and all of the users
+      // Keep in mind that everything you want to do AFTER you get the users from the database must
+      //   happen inside of this callback for it to be synchronous 
+      // Make sure you handle the case when there is an error, as well as the case when there is no error
+      .catch(console.log());
 
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    res.redirect('/');
+    })
+
+    //dummyArray=[{name: "bob, age: 45"}, {name:"fred", age: 33}];
+    //response.render('index',{users:dummyArray});
+
+
+
+
+
+
+// Use native promises (only necessary with mongoose versions <= 4)
+mongoose.Promise = global.Promise;
+
+app.post('/users', function(request, response) {
+    console.log("next line is console log post requst.body")
+    console.log("POST DATA", request.body);
+    // This is where we would add the user from request.body to the database.
+
+    var user = new User({name: request.body.name, age: request.body.age});
+    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+    user.save(function(err) {
+        // if there is an error console.log that something went wrong!
+        if(err) {
+            console.log('something went wrong');
+        } else { // else console.log that we did well and then redirect to the root route
+            console.log('successfully added a user!');
+        }
+        response.redirect('/');
+    })
 })
 
 
