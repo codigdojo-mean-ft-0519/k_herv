73c03e5d433130c1ab648c90fcfbc3c5ab99f936
Kent Hervey <kent.hervey@gmail.com>
2019-05-15 19:44:52 -0500

    Message Board complete

--- 04-mongodb/02-mongoose/03-message-board/server.js
+++ 04-mongodb/02-mongoose/03-message-board/server.js
@@ -12,42 +12,135 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost/dogs_db", { useNewUrlParser: true });  //establishes database. Name follows localhost/
+// app.use( function(request, response, next){
+//     console.log("middle url and body below");
+//     console.log(request.url);
+//     console.log(request.body);
+//     next();
+// });
+
+
+
+mongoose.connect("mongodb://localhost/message_board_db", { useNewUrlParser: true });  //establishes database. Name follows localhost/
 mongoose.connection.on("connected", () => console.log("Connected to Mongo!"));
 
 //Schemas/models/collections go below.  we need at least throw
 
-const DogSchema = new Schema({ //platform uses new mongoose.Schema, but we have const { Schema } = mongoose;
-    name:{
+const MessageSchema = new Schema({ //platform uses new mongoose.Schema, but we have const { Schema } = mongoose;
+    name:{  //This is the name of the message poster
         type: String,
-        required: [true, "Please enter in a name for your dog."]
+        required: [true, "Please enter your name."],
+        minlength: 2,
+        maxlength: 45,
+        trim: true
     },
-    breed:{
+    messagecontent:{  //Content of message
         type: String,
-        required: [true, "Breed is required"],
+        required: [true, "Message content is required"],
+        minlength: 2,
+       maxlength: 45,
+        trim: true
     },
-    age: {
-        type: Number,
-        required: true,
-        min: [0, "Dogs can't be negative ages"],
-        max: [23, "Dogs don't live longer than 23 years"],
-    }
-});
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+        required: false
+    }],
+},{timestamps: true}
+);
 
 
-const Dog = mongoose.model("Dog",DogSchema);
+var CommentSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        minlength: 2,
+        maxlength: 45,
+        trim: true,
+    },
+    commentcontent: {
+        type: String,
+        required: [true, "Please enter a quote"],
+        minlength:2,
+        maxlength: 225,
+        trim:true,
+    },
+    messagecontent_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Message',
+        required: true
+      }
+
+},{timestamps: true}
+);
+
+const Message = mongoose.model("Message",MessageSchema);
+const Comment = mongoose.model("Comment",CommentSchema);
+
+
+
+
 
 //Routes Below
 
 app.get("/", function (request, response) {
     //Code goes here
     console.log("test");
-    response.render('index');
+    Message.find({})
+        .populate("comments")
+        .then(messages => {
+            //console.log(messages)
+            response.render('index', {messages});
+        })
+    //let messagesDummy = [{name: "Fred", message: "This is a sample message"}, {name: "Mary", message: "A second sample message"}];
+        .catch(console.log);
+});
+
+// console.log("here")
+// Dog.find({ _id: request.params.id })
+//     .then(dog => {
+//         console.log(dog);
+//         response.render("dogs/show", {dog: dog});
+//     })
+    
+
+//route that creates message
+app.post("/createmessage", function(request,response){
+    console.log("request.body");
+    console.log(request.body);
+    Message.create(request.body)
+        .then(message => {
+            console.log("message below")
+            console.log(message.messagecontent);
+            response.redirect("/");
+        })
+        .catch(console.log);
+});
+
+//route that creates message
+app.post("/createcomment/:id", function(request,response){
+    console.log("request.params");
+    console.log(request.params);
+    Comment.create(request.body)
+        .then(comment => {
+            return Message.findByIdAndUpdate(comment.messagecontent_id, {$push: {comments: comment }})
+                .then(message => {
+                    console.log("Message updated with new comment attached!", message);
+                    response.redirect("/");
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            console.log(comment);
+            response.redirect("/");
+        })
+        .catch(console.log);
 });
 
 
+//route that creates comment on a certain message
 
-
+//redirects to /
 
 
 app.listen(port, () => console.log(`express server listening on port ${port}`));  //port defined above as port = process.env.PORT || 8000;
