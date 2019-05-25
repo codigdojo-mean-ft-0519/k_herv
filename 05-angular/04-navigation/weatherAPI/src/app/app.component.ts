e2136670ff4ecc81bf5ce528dd6be970ddc5aff1
Kent Hervey <kent.hervey@gmail.com>
2019-05-24 20:10:49 -0500

    links working again

--- 05-angular/04-navigation/weatherAPI/src/app/app.component.ts
+++ 05-angular/04-navigation/weatherAPI/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weatherAPI';
-}
+  //weathers = 999;
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    // this.weatherService.getWeather().subscribe(weather => {
+    //   console.log(weather.name);
+    // });
+  }
+} // End class
--- 05-angular/04-navigation/weatherAPI/src/app/burbank/burbank.component.ts
+++ 05-angular/04-navigation/weatherAPI/src/app/burbank/burbank.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-burbank',
   templateUrl: './burbank.component.html',
-  styleUrls: ['./burbank.component.css']
+  styleUrls: ['./burbank.component.css'],
 })
 export class BurbankComponent implements OnInit {
+  //@Input() weatherToShow: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
